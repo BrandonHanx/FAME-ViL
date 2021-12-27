8fd9d5e50abaf800d136af96dcec9679655103b5
BrandonHanx <brandon.hxiao@gmail.com>
2021-12-27 21:26:20 +0000

    [fix] condor submit file

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ HOME="/vol/research/xmodal_dl/mmf"
 echo $HOME
 echo 'args:' $@
 
+$PYTHONHOME/wandb login edc6324e6001d34f00aa0088ffbec6ff29180f2d
 $PYTHONHOME/python $HOME/mmf_cli/run.py env.data_dir=$HOME/data env.save_dir=$HOME/save $@
