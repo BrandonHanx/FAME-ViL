37ea5dd4f94cb65194d5bdb6ee468f3e418190f6
BrandonHanx <brandon.hxiao@gmail.com>
2022-09-16 22:33:08 +0100

    [feat] add option for learnable balances

--- mmf/models/fashionclip/mtl_kd.py
+++ mmf/models/fashionclip/mtl_kd.py
@@ -23,8 +23,10 @@ class FashionCLIPForMTLwithKD(FashionCLIPForMTL):
         super().__init__(config)
         self.teachers = nn.ModuleDict()
         self.kd_loss_funcs = nn.ModuleDict()
+        self.balances = nn.ParameterDict()
         self.init_teachers()
         self.init_kd_losses()
+        self.init_balances()
 
     @staticmethod
     def _rename_state_dict(state_dict):
@@ -100,14 +102,37 @@ class FashionCLIPForMTLwithKD(FashionCLIPForMTL):
         if "cap" in self.tasks:
             self.kd_loss_funcs["cap"] = SoftLabelCrossEntropyLoss()
 
+    def init_balances(self):
+        learnable_balances = self.config.get("learnable_balances", False)
+        if "itc" in self.tasks:
+            self.balances["itc"] = nn.Parameter(
+                torch.ones(1), requires_grad=learnable_balances
+            )
+        if "tgir" in self.tasks:
+            self.balances["tgir"] = nn.Parameter(
+                torch.ones(1), requires_grad=learnable_balances
+            )
+        if "scr" in self.tasks:
+            self.balances["scr"] = nn.Parameter(
+                torch.ones(1), requires_grad=learnable_balances
+            )
+        if "cap" in self.tasks:
+            self.balances["cap"] = nn.Parameter(
+                torch.ones(1), requires_grad=learnable_balances
+            )
+
     def _forward_itc(self, sample_list: Dict[str, Tensor]) -> Dict[str, Tensor]:
         output_dict = super()._forward_itc(sample_list)
         if self.training:
             teacher_output_dict = self.teachers["itc"]._forward_itc(sample_list)
             output_dict["teacher_scores"] = teacher_output_dict["scores"]
             output_dict["teacher_targets"] = teacher_output_dict["targets"]
-            output_dict["losses"]["kd_itc_loss"] = self.kd_loss_funcs["itc"](
-                sample_list, output_dict
+            output_dict["losses"]["kd_itc_loss"] = (
+                self.kd_loss_funcs["itc"](sample_list, output_dict)
+                * self.balances["itc"]
+            )
+            output_dict["losses"]["itc_loss"] = output_dict["losses"]["itc_loss"] * (
+                2 - self.balances["itc"]
             )
         return output_dict
 
@@ -117,8 +142,12 @@ class FashionCLIPForMTLwithKD(FashionCLIPForMTL):
             teacher_output_dict = self.teachers["tgir"]._forward_tgir(sample_list)
             output_dict["teacher_comp_feats"] = teacher_output_dict["comp_feats"]
             output_dict["teacher_tar_feats"] = teacher_output_dict["tar_feats"]
-            output_dict["losses"]["kd_tgir_loss"] = self.kd_loss_funcs["tgir"](
-                sample_list, output_dict
+            output_dict["losses"]["kd_tgir_loss"] = (
+                self.kd_loss_funcs["tgir"](sample_list, output_dict)
+                * self.balances["tgir"]
+            )
+            output_dict["losses"]["tgir_loss"] = output_dict["losses"]["tgir_loss"] * (
+                2 - self.balances["tgir"]
             )
         return output_dict
 
@@ -127,8 +156,12 @@ class FashionCLIPForMTLwithKD(FashionCLIPForMTL):
         if self.training:
             teacher_output_dict = self.teachers["scr"]._forward_scr(sample_list)
             sample_list["targets"] = F.softmax(teacher_output_dict["scores"], dim=-1)
-            output_dict["losses"]["kd_scr_loss"] = self.kd_loss_funcs["scr"](
-                sample_list, output_dict
+            output_dict["losses"]["kd_scr_loss"] = (
+                self.kd_loss_funcs["scr"](sample_list, output_dict)
+                * self.balances["scr"]
+            )
+            output_dict["losses"]["scr_loss"] = output_dict["losses"]["scr_loss"] * (
+                2 - self.balances["scr"]
             )
         return output_dict
 
@@ -137,7 +170,11 @@ class FashionCLIPForMTLwithKD(FashionCLIPForMTL):
         if self.training:
             teacher_output_dict = self.teachers["cap"]._forward_cap(sample_list)
             sample_list["targets"] = F.softmax(teacher_output_dict["scores"], dim=-1)
-            output_dict["losses"]["kd_cap_loss"] = self.kd_loss_funcs["cap"](
-                sample_list, output_dict
+            output_dict["losses"]["kd_cap_loss"] = (
+                self.kd_loss_funcs["cap"](sample_list, output_dict)
+                * self.balances["cap"]
+            )
+            output_dict["losses"]["cap_loss"] = output_dict["losses"]["cap_loss"] * (
+                2 - self.balances["cap"]
             )
         return output_dict
